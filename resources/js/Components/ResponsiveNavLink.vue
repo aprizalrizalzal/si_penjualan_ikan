129018aae87acaf90ea68343d64733a79b1a0a6a
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-08-20 19:39:03 +0800

    update responsivenavlink

--- resources/js/Components/ResponsiveNavLink.vue
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'block w-full ps-3 pe-4 py-6 border-l-4 border-blue-400 text-start text-base font-medium text-gray-700 focus:outline-none focus:text-gray-800 focus:border-blue-700 transition duration-250 ease-in-out'
+        ? 'block w-full ps-3 pe-4 py-6 border-l-4 border-blue-400 text-start text-base font-medium text-gray-700 bg-blue-100 focus:outline-none focus:text-gray-800 focus:bg-blue-100 focus:border-blue-700 transition duration-250 ease-in-out'
         : 'block w-full ps-3 pe-4 py-3 border-l-4 border-transparent text-start text-base font-sm text-gray-600 hover:text-gray-800 hover:bg-blue-100 hover:border-blue-300 focus:outline-none focus:text-gray-800 focus:bg-blue-100 focus:border-blue-300 transition duration-250 ease-in-out'
 );
 </script>
