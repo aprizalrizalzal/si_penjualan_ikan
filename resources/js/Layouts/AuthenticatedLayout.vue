e4fd35f9ab9794d9f634a2f978f600a8e4ddbd48
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-08-20 12:22:31 +0800

    add stroke-width

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -43,7 +43,7 @@ onUnmounted(() => {
                             aria-controls="drawer-navigation">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                                 class="bi bi-chevron-compact-right animate-pulse" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
+                                <path fill-rule="evenodd" stroke="currentColor" stroke-width="0.5"
                                     d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671" />
                             </svg>
                         </button>
