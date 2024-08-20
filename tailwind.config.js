9c4ba9badb8e25855a1ccd18b5f37139ec51ede1
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2024-08-20 08:35:14 +0800

    add plugin flowbite, keyframes animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -15,8 +16,40 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                'fade-down': {
+                  '0%': { opacity: '0', transform: 'translateY(-20px)' },
+                  '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+                'fade-up': {
+                  '0%': { opacity: '0', transform: 'translateY(20px)' },
+                  '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+                'fade-right': {
+                  '0%': { opacity: '0', transform: 'translateX(-20px)' },
+                  '100%': { opacity: '1', transform: 'translateX(0)' },
+                },
+                'fade-left': {
+                  '0%': { opacity: '0', transform: 'translateX(20px)' },
+                  '100%': { opacity: '1', transform: 'translateX(0)' },
+                },
+                'fade': {
+                  '0%': { opacity: '0' },
+                  '100%': { opacity: '1' },
+                },
+              },
+              animation: {
+                'fade-down': 'fade-down 1s ease-out',
+                'fade-up': 'fade-up 1s ease-out',
+                'fade-right': 'fade-right 1s ease-out',
+                'fade-left': 'fade-left 1s ease-out',
+                'fade': 'fade 1s ease-out',
+              },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
